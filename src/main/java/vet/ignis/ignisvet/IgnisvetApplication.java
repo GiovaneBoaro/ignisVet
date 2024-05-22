3f45927043ebe7bc6e58ca57b229abfa2b360636
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-05-22 11:08:47 -0300

    first commit

---
+++ src/main/java/vet/ignis/ignisvet/IgnisvetApplication.java
@@ -0,0 +1,13 @@
+package vet.ignis.ignisvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnisvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgnisvetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vet/ignis/ignisvet/IgnisvetApplicationTests.java
@@ -0,0 +1,13 @@
+package vet.ignis.ignisvet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgnisvetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
