444320cb778be4f9d9b199a59f9fe94a93ca5f57
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-05-24 20:04:43 -0300

    feat(controller): adiciona classe Veterinario com anotacao @RestController e @RequestMapping passando a url da requisição

---
+++ src/main/java/vet/ignis/ignisvet/controller/VeterinarioController.java
@@ -0,0 +1,12 @@
+package vet.ignis.ignisvet.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("veterinarios")
+public class VeterinarioController {
+
+
+}
