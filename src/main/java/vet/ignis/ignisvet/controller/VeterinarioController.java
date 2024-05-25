85f68a718fed2e2d9792865337f63760c1fb89be
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-05-24 21:02:00 -0300

    feat(controller): utiliza anotacao @RequestBody e o record VeterinarioDTO para printar dados e testar requisicao

--- src/main/java/vet/ignis/ignisvet/controller/VeterinarioController.java
+++ src/main/java/vet/ignis/ignisvet/controller/VeterinarioController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vet.ignis.ignisvet.model.veterinario.VeterinarioDTO;
 
 
 @RestController
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class VeterinarioController {
 
     @PostMapping
-    public void cadastrar() {
-
+    public void cadastrar(@RequestBody VeterinarioDTO dados) {
+        System.out.println(dados);
     }
 
 }
