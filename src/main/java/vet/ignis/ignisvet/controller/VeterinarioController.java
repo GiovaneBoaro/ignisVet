05308901e49f2e03df362bc06d4122450d69ca43
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-05-29 19:23:44 -0300

    feat(controller): cria atributo com injeção de dependencias utilizando a tag @autowired para o spring instancionar e passar o atributo repository para a classe controller

--- src/main/java/vet/ignis/ignisvet/controller/VeterinarioController.java
+++ src/main/java/vet/ignis/ignisvet/controller/VeterinarioController.java
@@ -1,19 +1,24 @@
 package vet.ignis.ignisvet.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vet.ignis.ignisvet.model.veterinario.VeterinarioDTO;
+import vet.ignis.ignisvet.repository.VeterinarioRepository;
 
 
 @RestController
 @RequestMapping("veterinarios")
 public class VeterinarioController {
 
+    @Autowired
+    private VeterinarioRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody VeterinarioDTO dados) {
-        System.out.println(dados);
+
     }
 
 }
