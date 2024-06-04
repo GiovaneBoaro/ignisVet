2d150dc53979d093ee1276b8f7ad0decd57e26cc
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-06-04 14:49:52 -0300

    feat(model): adiciona anotações do bean validation nos atributos do EnderecoDTO

--- src/main/java/vet/ignis/ignisvet/model/endereco/EnderecoDTO.java
+++ src/main/java/vet/ignis/ignisvet/model/endereco/EnderecoDTO.java
@@ -1,4 +1,26 @@
 package vet.ignis.ignisvet.model.endereco;
 
-public record EnderecoDTO(String logradouro, String numero, String complemento, String cidade, String uf, String cep) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record EnderecoDTO (
+
+        @NotBlank
+        String logradouro,
+
+
+        String numero,
+
+
+        String complemento,
+
+        @NotBlank
+        String cidade,
+
+        @NotBlank
+        String uf,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String cep) {
 }
