418268ac47282ce44abecbe3f7b82597ee31b6ca
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-06-04 14:46:56 -0300

    feat(model): adiciona anotações do bean validation nos atributos do VeterinarioDTO para verificar se as informacoes que chegam estao de acordo

--- src/main/java/vet/ignis/ignisvet/model/veterinario/VeterinarioDTO.java
+++ src/main/java/vet/ignis/ignisvet/model/veterinario/VeterinarioDTO.java
@@ -1,6 +1,32 @@
 package vet.ignis.ignisvet.model.veterinario;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import vet.ignis.ignisvet.model.endereco.EnderecoDTO;
 
-public record VeterinarioDTO(String nome, Especialidade especialidade, String email, String celular, String crmv, EnderecoDTO endereco) {
+public record VeterinarioDTO(
+
+        @NotBlank
+        String nome,
+
+        @NotNull
+        Especialidade especialidade,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String celular,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{6,7}")
+        String crmv,
+
+        @NotNull
+        @Valid
+        EnderecoDTO endereco) {
 }
