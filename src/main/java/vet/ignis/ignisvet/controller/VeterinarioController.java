e715a70d5c456a0619f5d675a447a7f6ab6af2de
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-06-04 12:20:28 -0300

    feat(controller): adiciona anotação @Transactional no metodo cadastrar para captar uma transaçao ativa com o banco de dados

--- src/main/java/vet/ignis/ignisvet/controller/VeterinarioController.java
+++ src/main/java/vet/ignis/ignisvet/controller/VeterinarioController.java
@@ -1,6 +1,7 @@
 package vet.ignis.ignisvet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class VeterinarioController {
     private VeterinarioRepository repository;
 
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody VeterinarioDTO dados) {
         repository.save(new Veterinario(dados));
     }
