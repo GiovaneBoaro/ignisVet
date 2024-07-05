9121c81f5e68ed558325ab1692ebf1967388df4e
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-05 20:16:08 -0300

    perd(Paciente/controller): adiciona repository com o metodo save para criar um novo objeto entidade jpa paciente passando os parametros que vem do json

--- src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
+++ src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vet.ignis.ignisvet.model.Paciente;
 import vet.ignis.ignisvet.model.PacienteDTO;
 import vet.ignis.ignisvet.repository.PacienteRepository;
 
@@ -17,6 +18,6 @@ public class PacienteController {
 
     @PostMapping
     public void cadastrar(@RequestBody PacienteDTO dados) {
-
+        repository.save(new Paciente(dados));
     }
 }
