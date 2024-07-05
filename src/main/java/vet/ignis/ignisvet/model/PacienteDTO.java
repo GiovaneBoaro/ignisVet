a2652ad0e4d2ee5c586f1a5e66e2bf51c598bbf6
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-05 14:29:11 -0300

    feat(Paciente/Model): adiciona classe DTO para paciente

---
+++ src/main/java/vet/ignis/ignisvet/model/PacienteDTO.java
@@ -0,0 +1,5 @@
+package vet.ignis.ignisvet.model;
+
+public record PacienteDTO(String nomeAnimal, int idadeAnimal, Especie especieAnimal, String nomeDono, String emailDono, String telefoneDono, String cpfDono) {
+
+}
