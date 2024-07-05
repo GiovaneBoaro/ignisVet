a429eaec844d46990d5349fbbd089f87fb7120f3
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-05 14:21:44 -0300

    feat(Paciente/Controller): adiciona metodo cadastrar na classe Paciente

--- src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
+++ src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
@@ -1,5 +1,6 @@
 package vet.ignis.ignisvet.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("pacientes")
 public class PacienteController {
 
+    @PostMapping
+    public void cadastrar() {
+
+    }
 }
