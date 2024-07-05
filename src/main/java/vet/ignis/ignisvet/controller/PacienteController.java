bee3b9fa3f6748d70362111fb78be56887c462b2
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-05 20:26:46 -0300

    feat(Paciente/controller): adiciona tag no metodo cadastrar para captar uma transacao ativa com o banco

--- src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
+++ src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
@@ -1,6 +1,7 @@
 package vet.ignis.ignisvet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class PacienteController {
     private PacienteRepository repository;
 
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody PacienteDTO dados) {
         repository.save(new Paciente(dados));
     }
