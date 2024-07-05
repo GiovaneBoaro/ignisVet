8863115c8df766fdba655359329dc5eba7ec2a7b
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-05 19:54:00 -0300

    feat(Paciente/model): adiciona entidade jpa

---
+++ src/main/java/vet/ignis/ignisvet/model/Paciente.java
@@ -0,0 +1,14 @@
+package vet.ignis.ignisvet.model;
+
+public class Paciente {
+
+    private Long id;
+    private String nomeAnimal;
+    private int idadeAnimal;
+    private Especie especieAnimal;
+    private String nomeDono;
+    private String emailDono;
+    private String telefoneDono;
+    private String cpfDono;
+
+}
