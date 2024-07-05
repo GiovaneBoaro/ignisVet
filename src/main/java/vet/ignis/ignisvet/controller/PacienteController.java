a1a340f41ced20952f1063ded626a749695b3424
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-05 20:08:32 -0300

    feat(Paciente/controller): atributo com injecao de dependencias utilizando tag para instanciar e passar o atributo para a controller

--- src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
+++ src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
@@ -1,14 +1,19 @@
 package vet.ignis.ignisvet.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vet.ignis.ignisvet.model.PacienteDTO;
+import vet.ignis.ignisvet.repository.PacienteRepository;
 
 @RestController
 @RequestMapping("pacientes")
 public class PacienteController {
+
+    @Autowired
+    private PacienteRepository repository;
 
     @PostMapping
     public void cadastrar(@RequestBody PacienteDTO dados) {
