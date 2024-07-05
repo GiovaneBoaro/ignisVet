4a070ed99a43c35c00d128ed60cfd7d0a5634a10
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-05 19:59:22 -0300

    feat(Paciente/model): mapeando entidade jpa usando tags

--- src/main/java/vet/ignis/ignisvet/model/Paciente.java
+++ src/main/java/vet/ignis/ignisvet/model/Paciente.java
@@ -1,14 +1,22 @@
 package vet.ignis.ignisvet.model;
 
+import jakarta.persistence.*;
+
+@Table (name = "pacientes")
+@Entity (name = "Paciente")
 public class Paciente {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nomeAnimal;
     private int idadeAnimal;
+
+    @Enumerated(EnumType.STRING)
     private Especie especieAnimal;
+
     private String nomeDono;
     private String emailDono;
     private String telefoneDono;
     private String cpfDono;
-
 }
