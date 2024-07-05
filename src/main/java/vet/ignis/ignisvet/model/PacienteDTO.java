259771d887d04c64f9d99e8241853b9b09ee510c
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-05 20:31:03 -0300

    feat(Paciente/model): adiciona anotacoes do bean validation nos atributos do pacienteDTO

--- src/main/java/vet/ignis/ignisvet/model/PacienteDTO.java
+++ src/main/java/vet/ignis/ignisvet/model/PacienteDTO.java
@@ -1,5 +1,34 @@
 package vet.ignis.ignisvet.model;
 
-public record PacienteDTO(String nomeAnimal, int idadeAnimal, Especie especieAnimal, String nomeDono, String emailDono, String telefoneDono, String cpfDono) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.br.CPF;
+
+public record PacienteDTO(
+
+        @NotBlank
+        String nomeAnimal,
+
+        @NotBlank
+        int idadeAnimal,
+
+        @NotNull
+        Especie especieAnimal,
+
+        @NotBlank
+        String nomeDono,
+
+        @NotBlank
+        @Email
+        String emailDono,
+
+        @NotBlank
+        String telefoneDono,
+
+        @NotBlank
+        @CPF
+        String cpfDono) {
 
 }
