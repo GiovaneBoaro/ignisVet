115aca80782566325011915d169d0e1ee5ad5d7a
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-05 20:32:36 -0300

    feat(Paciente/controller): atribui tag valid no parametro dto do metodo cadastrar para integracao do spring com o bean validation e executar as validacoes

--- src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
+++ src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
@@ -1,5 +1,6 @@
 package vet.ignis.ignisvet.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ public class PacienteController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody PacienteDTO dados) {
+    public void cadastrar(@RequestBody @Valid PacienteDTO dados) {
         repository.save(new Paciente(dados));
     }
 }
