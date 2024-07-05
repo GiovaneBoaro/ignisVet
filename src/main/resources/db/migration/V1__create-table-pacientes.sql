f72efab3350148c0ec6d628c846c5e1703809656
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-05 20:25:35 -0300

    feat(Paciente): adiciona migration para criar tabela paciente

---
+++ src/main/resources/db/migration/V1__create-table-pacientes.sql
@@ -0,0 +1,14 @@
+create table pacientes {
+
+    id bigint not null auto_increment,
+    nomeAnimal varchar(100) not null,
+    idadeAnimal int not null,
+    especieAnimal varchar(100) not null,
+    nomeDono varchar(100) not null,
+    emailDono varchar(100) not null unique,
+    telefoneDono varchar(100) not null,
+    cpfDono varchar(11) not null unique,
+
+    primary key(id)
+
+};
