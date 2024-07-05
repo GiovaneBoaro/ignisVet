c1feeba232471d40a2e414cafcd7200399949826
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-05 14:34:39 -0300

    feat(Paciente/Controller): chama o DTO no metodo cadastrar

--- src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
+++ src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
@@ -1,15 +1,17 @@
 package vet.ignis.ignisvet.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vet.ignis.ignisvet.model.PacienteDTO;
 
 @RestController
 @RequestMapping("pacientes")
 public class PacienteController {
 
     @PostMapping
-    public void cadastrar() {
+    public void cadastrar(@RequestBody PacienteDTO dados) {
 
     }
 }
