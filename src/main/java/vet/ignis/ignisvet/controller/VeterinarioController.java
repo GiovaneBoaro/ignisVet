240fc5352763c6546875c587a0d1e30e6bd7f972
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-12 14:14:14 -0300

    feat(Veterinario/controller): cria metodo de listagem na classe controller

--- src/main/java/vet/ignis/ignisvet/controller/VeterinarioController.java
+++ src/main/java/vet/ignis/ignisvet/controller/VeterinarioController.java
@@ -3,14 +3,14 @@ package vet.ignis.ignisvet.controller;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import vet.ignis.ignisvet.model.endereco.Endereco;
+import vet.ignis.ignisvet.model.veterinario.ListagemVeterinarioDTO;
 import vet.ignis.ignisvet.model.veterinario.Veterinario;
 import vet.ignis.ignisvet.model.veterinario.VeterinarioDTO;
 import vet.ignis.ignisvet.repository.VeterinarioRepository;
+
+import java.util.List;
 
 
 @RestController
@@ -24,6 +24,11 @@ public class VeterinarioController {
     @Transactional
     public void cadastrar(@RequestBody @Valid VeterinarioDTO dados) {
         repository.save(new Veterinario(dados));
+    }
+
+    @GetMapping
+    public List<ListagemVeterinarioDTO> listar() {
+        return repository.findAll();
     }
 
 }
