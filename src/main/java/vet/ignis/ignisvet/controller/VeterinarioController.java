d3a6d4a8fddd2644e0418e525e345cd19f0cfb3f
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-15 20:43:28 -0300

    perf(Veterinario/controller): adiciona tag que define ordenacao padrao para a listagem

--- src/main/java/vet/ignis/ignisvet/controller/VeterinarioController.java
+++ src/main/java/vet/ignis/ignisvet/controller/VeterinarioController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import vet.ignis.ignisvet.model.veterinario.ListagemVeterinarioDTO;
@@ -28,7 +29,7 @@ public class VeterinarioController {
     }
 
     @GetMapping
-    public Page<ListagemVeterinarioDTO> listar(Pageable paginacao) {
+    public Page<ListagemVeterinarioDTO> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return repository.findAll(paginacao).map(ListagemVeterinarioDTO::new);
     }
 
