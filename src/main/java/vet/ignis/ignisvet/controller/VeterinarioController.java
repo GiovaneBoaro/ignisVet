7dba7e3ef730b3e59c97b20fd91c0b47d768d904
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-15 20:27:10 -0300

    perf(Veterinario/controller): muda metodo de listar para adicionar funcao de paginacao para a listagem

--- src/main/java/vet/ignis/ignisvet/controller/VeterinarioController.java
+++ src/main/java/vet/ignis/ignisvet/controller/VeterinarioController.java
@@ -2,6 +2,8 @@ package vet.ignis.ignisvet.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import vet.ignis.ignisvet.model.veterinario.ListagemVeterinarioDTO;
@@ -26,8 +28,8 @@ public class VeterinarioController {
     }
 
     @GetMapping
-    public List<ListagemVeterinarioDTO> listar() {
-        return repository.findAll().stream().map(ListagemVeterinarioDTO::new).toList();
+    public Page<ListagemVeterinarioDTO> listar(Pageable paginacao) {
+        return repository.findAll(paginacao).map(ListagemVeterinarioDTO::new);
     }
 
 }
