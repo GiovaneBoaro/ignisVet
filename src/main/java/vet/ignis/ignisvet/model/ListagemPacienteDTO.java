baa50f605bbf54c70ca13cd3f5b3a334bfb67218
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-15 21:17:36 -0300

    feat(Paciente/model): cria construtor dentro da classe DTO de listagem para especificar os atributos que serao mostrados

--- src/main/java/vet/ignis/ignisvet/model/ListagemPacienteDTO.java
+++ src/main/java/vet/ignis/ignisvet/model/ListagemPacienteDTO.java
@@ -1,5 +1,8 @@
 package vet.ignis.ignisvet.model;
 
-public record ListagemPacienteDTO(String nome_animal, String especie_animal, int idade_animal, String nome_dono) {
+public record ListagemPacienteDTO(String nome_animal, Especie especie, int idade_animal, String nome_dono) {
 
+    public ListagemPacienteDTO(Paciente paciente) {
+        this(paciente.getNome_animal(), paciente.getEspecie_animal(), paciente.getIdade_animal(), paciente.getNome_dono());
+    }
 }
