7ec69f0aa2875c885d8ffe599d33acefd1f23a7b
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2024-07-15 21:12:14 -0300

    feat(Paciente/controller): cria metodo de listagem na classe controller

--- src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
+++ src/main/java/vet/ignis/ignisvet/controller/PacienteController.java
@@ -3,13 +3,13 @@ package vet.ignis.ignisvet.controller;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import vet.ignis.ignisvet.model.ListagemPacienteDTO;
 import vet.ignis.ignisvet.model.Paciente;
 import vet.ignis.ignisvet.model.PacienteDTO;
 import vet.ignis.ignisvet.repository.PacienteRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("pacientes")
@@ -23,4 +23,10 @@ public class PacienteController {
     public void cadastrar(@RequestBody @Valid PacienteDTO dados) {
         repository.save(new Paciente(dados));
     }
+
+    @GetMapping
+    public List<ListagemPacienteDTO> listar() {
+        return repository.findAll();
+    }
+
 }
